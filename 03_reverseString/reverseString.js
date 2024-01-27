678bcd66022dd69e42be479d39f5e95a56d55665
danjpastor <147651772+danjpastor@users.noreply.github.com>
2024-01-27 17:37:11 -0500

    Initial Commit, Complete First Three Exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = []
+    for (let i = 0; i < string.length; i++) {
+        stringArray.push(string[i])
+    }
+    stringArray.reverse()
+    return stringArray.join("")
 
 };
+
+console.log(reverseString("hello"))
 
 // Do not edit below this line
 module.exports = reverseString;
