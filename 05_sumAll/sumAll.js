becf5c21e766251ffada8906d388e141d3dd2db3
danjpastor <147651772+danjpastor@users.noreply.github.com>
2024-01-28 11:15:57 -0500

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let rangeSum = 0
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    if (num1 > num2) {
+        for (let i = num1; i >= num2; i-- ) {
+            rangeSum += i
+            console.log(rangeSum)
+        }    
+    } else {
+        for (let i = num1; i <= num2; i++ ) {
+            rangeSum += i
+            console.log(rangeSum)
+        }    
+    }
+    return rangeSum
 };
+
+console.log(sumAll(10, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
