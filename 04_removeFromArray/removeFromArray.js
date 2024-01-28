ba9596eef22bad2e0b916b030bb45e4eb82c7506
danjpastor <147651772+danjpastor@users.noreply.github.com>
2024-01-27 19:17:51 -0500

    Finish Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
 const removeFromArray = function() {
 
-};
+let argArray = [];
+let givenArray = arguments[0];
 
-// Do not edit below this line
+for (i = 0; i < arguments.length; i++) {
+    if (i != 0) {
+        argArray.push(arguments[i])
+    }
+};
+// ----------------------------------------
+
+const newArray = givenArray.filter(function(x) { 
+    return argArray.indexOf(x) < 0;
+  });
+
+  return newArray;
+}
+
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
+
+
+// // Do not edit below this line
 module.exports = removeFromArray;
