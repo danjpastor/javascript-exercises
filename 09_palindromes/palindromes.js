6c660f428a21560acbe918e969c3a447f462edb5
danjpastor <147651772+danjpastor@users.noreply.github.com>
2024-02-04 18:43:18 -0500

    Finish Exercise 08

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (orgstring) {
+    let string = orgstring.toLowerCase();
+    let replace = string.replace(/[^a-z0-9]/gi, '').split('');
+    let reverse = replace.reverse();
+    let newstring = reverse.join('');
+    if (newstring === string.replace(/[^a-z0-9]/gi, '')){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+
+// let string = "bob";
+// let replace = string.replace(/[^a-z0-9]/gi, '').split('');
+// // console.log(replace.join(''))
+// let reverse = replace.reverse();
+// let newstring = reverse.join('');
+// if (newstring == replace.join('')){
+//     console.log("true")
+// }
