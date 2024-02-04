5f12b7bc35d5b9e5084d1774837dedce9d385f80
danjpastor <147651772+danjpastor@users.noreply.github.com>
2024-02-04 18:19:54 -0500

    Finish Exercise 07

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+	for (let i = 0; i < array.length; i++){
+    sum += array[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = array[0]
+	for (let i = 1; i < array.length; i++){
+    product *= array[i]
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num == 0){
+    return 1
+  } else {
+    for (i = num-1; i >= 1; i--){
+      result *= i
+    }
+  }
+  return result
 };
 
 // Do not edit below this line
