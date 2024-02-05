c2320b840329907ddc576ea7dbdc41210224e403
danjpastor <147651772+danjpastor@users.noreply.github.com>
2024-02-05 15:57:58 -0500

    Finish Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    if (num == 0) {
+        return 0
+    } else if (num < 0){
+        return "OOPS"
+    } else {
+        let num1 = 0;
+        let num2 = 1;
+        let sum;
+        for (i = 1; i< num; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2
+    }
 };
 
-// Do not edit below this line
+// // Do not edit below this line
 module.exports = fibonacci;
+
+
+    
